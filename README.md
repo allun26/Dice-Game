fa8c6370217b3d4796fc4cac177dfb706ac45d92
Alexander Einshøj Lund <alexander.lund1@gmail.com>
2019-04-30 14:55:53 +0200

    added an extra dice

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Dice Game coded in javascript
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- In each turn, a player rolls two dices as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- If the player rolls two 6'es in a row his round score, AND global score resets to 0, and it is the next players turn
+- If the player rolls two 6'es his round score, AND global score resets to 0, and it is the next players turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 - If you want to play to more than 100 points, you can change it in the input field "Final Score"
--- app.js
+++ app.js
@@ -10,7 +10,7 @@ GAME RULES:
 */
 
 
-let scores, roundScore, activePlayer, gamePlaying, two6;
+let scores, roundScore, activePlayer, gamePlaying;
 //calls function to initialize the game
 window.addEventListener('load', init);
 
@@ -19,22 +19,25 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     if(gamePlaying){
     
         //creates random number
-        let dice = Math.floor(Math.random() * 6 ) + 1;
+        let dice1 = Math.floor(Math.random() * 6 ) + 1;
+        let dice2 = Math.floor(Math.random() * 6 ) + 1;
         
         
-            if (two6 === 6 && dice === 6){
+            if (dice1 === 6 && dice2 === 6){
                 scores[activePlayer] = 0;
                 document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
                 nextPlayer();
             }else {
                 //display the result
-                let diceDOM = document.querySelector('.dice');
-                diceDOM.style.display = 'block';
-                diceDOM.src = 'Images/dice-' + dice + '.png';
-                //adds current score if dice is not 1, and change turn and reset current score if dice is 1
-                if(dice !== 1){
-                    two6 = dice;
-                    roundScore += dice;
+                let diceDOM1 = document.querySelector('#dice-1');
+                let diceDOM2 = document.querySelector('#dice-2');
+                diceDOM1.style.display = 'block';
+                diceDOM1.src = 'Images/dice-' + dice1 + '.png';
+                diceDOM2.style.display = 'block';
+                diceDOM2.src = 'Images/dice-' + dice2 + '.png';
+                //adds current score if none of the dices are 1, - change turn and reset current score if one of the dices are 1
+                if(dice1 !== 1 && dice2 !== 1){
+                    roundScore += dice1 + dice2;
                     document.querySelector('#current-' + activePlayer).textContent = roundScore;
                 } else {
                     //next Player
@@ -66,7 +69,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         //check if player won the game
         if (scores[activePlayer] >= winnerScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('#dice-1').style.display = 'none';
+            document.querySelector('#dice-2').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
@@ -88,7 +92,9 @@ function nextPlayer(){
         document.querySelector('.player-0-panel').classList.toggle('active')
         document.querySelector('.player-1-panel').classList.toggle('active')
 
-        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('#dice-1').style.display = 'none';
+        document.querySelector('#dice-2').style.display = 'none';
+
 }
 
 // adds event listener to New game button, and initialize new game
@@ -100,9 +106,9 @@ function init() {
     roundScore = 0;
     activePlayer = 0;
     gamePlaying = true;
-    two6 = 0;
 
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('#dice-1').style.display = 'none';
+    document.querySelector('#dice-2').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
